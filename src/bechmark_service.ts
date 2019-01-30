c0038da585f553e97d192ce3c3bb82deac5f8f96
kentrino <tachikoma@h-kento.jp>
2019-01-31 04:00:39 +0900

    refactor

---
+++ src/bechmark_service.ts
@@ -0,0 +1,48 @@
+import { uuid, benchmark } from "./util"
+import { User } from "./user"
+import { NeoUser } from "./neo_user"
+import { ConnectionOptions, createConnection, Repository, ObjectLiteral, DeepPartial } from "typeorm"
+import { options } from "./db"
+import { Constructable } from "./user_factory"
+
+type Id = string | number
+
+export class BenchmarkService<T extends NeoUser | User> {
+  constructor(
+    private prefix: string,
+    private repository: Repository<T>,
+    private factory: Constructable<T>,
+    private smallBachSize: number,
+    public largeBatchSize: number,
+  ) {}
+
+  async createUser(i: number, repository: Repository<T>): Promise<T> {
+    const user = new this.factory(i)
+    await repository.save(user as any)
+    return user
+  }
+
+  async getUserById(ids: Id[], repository: Repository<T>): Promise<T[]> {
+    const users = await repository.findByIds(ids)
+    return users as T[]
+  }
+
+  async benchmarkCreateFind10(start: number) {
+    const thousandUsers: T[] = []
+    for (let i = start; i < start + this.largeBatchSize; i += this.smallBachSize) {
+      await benchmark(`${this.prefix}_create_${this.smallBachSize}`, async () => {
+        const users: T[] = []
+        for (let j = i; j < i + this.smallBachSize; ++j) {
+          const user = new this.factory(j) as T
+          users.push(user)
+          thousandUsers.push(user)
+        }
+        await this.repository.save(users as any)
+      })
+    }
+    const ids = thousandUsers.map((u) => u.id)
+    await benchmark(`${this.prefix}_find_${this.largeBatchSize}`, async () => {
+      await this.getUserById(ids, this.repository)
+    })
+  }
+}
--- user.sql
+++ user.sql
@@ -13,3 +13,19 @@ CREATE TABLE `users` (
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `neo_users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `family_name` varchar(255) NOT NULL,
+  `given_name` varchar(255) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `gender` int(11) NOT NULL,
+  `birth_date` datetime NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
